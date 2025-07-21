ecc76c9f4f90798ebc64a0b24db127c0c398cfb1
Seif Eldien Ahmad Mohammad <seifahmed7293@gmail.com>
2025-07-21 22:10:59 +0300

    Update rsourcesChecker.sh

--- rsourcesChecker.sh
+++ rsourcesChecker.sh
@@ -13,19 +13,19 @@ CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 RAM=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
 
 # Log the date, CPU, and RAM usage to a log file
-echo "$(date '+%Y-%m-%d  %H:%M:%S') - CPU: ${CPU}% - RAM: ${RAM}%" >> "$HOME/Desktop/Scripts/resourcesChecker/usageLog.txt"
+echo "$(date '+%Y-%m-%d  %H:%M:%S') - CPU: ${CPU}% - RAM: ${RAM}%" >> "your/path/to/usageLog.txt"
 
 # Check if CPU usage exceeds 80%, and print a warning if it does
 if [ $(echo "$CPU > 80" | bc -l) -eq 1 ]
 then
-    echo "High CPU usage: $CPU%" | mail -s "CPU Alert from $(hostname)" seifahmed7293@gmail.com
+    echo "High CPU usage: $CPU%" | mail -s "CPU Alert from $(hostname)" yourE-mail
     notify-send "High CPU usage!" "CPU usage is ${CPU}%"
 fi
 
 # Check if RAM usage exceeds 80%, and print a warning if it does
 if [ $(echo "$RAM > 80" | bc -l) -eq 1 ]
 then
-    echo "High RAM usage: $RAM%" | mail -s "RAM Alert from $(hostname)" seifahmed7293@gmail.com
+    echo "High RAM usage: $RAM%" | mail -s "RAM Alert from $(hostname)" yourE-mail
     notify-send "High RAM usage!" "RAM usage is ${RAM}%"
 fi
 
